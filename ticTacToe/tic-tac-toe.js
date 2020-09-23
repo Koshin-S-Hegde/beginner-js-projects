21cece78a21439aee9a71accb4b6579402cbea71
Koshin-S-Hegde <71308357+Koshin-S-Hegde@users.noreply.github.com>
2020-09-23 13:07:49 +0530

    Made some more changes

--- ticTacToe/tic-tac-toe.js
+++ ticTacToe/tic-tac-toe.js
@@ -1,64 +1,92 @@
 let board = [ [' ', ' ', ' '],
-              [' ', ' ', ' '],
-              [' ', ' ', ' '] ];
+	[' ', ' ', ' '],
+	[' ', ' ', ' '] ];
 
 let currentPlayer;
 
+let board_position;
+
 const printBoardPositions = () => {
-  console.log('Here is the current board...');
-  // prints positions for user
-  let currentNumber = 1;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-        board[i][j] = currentNumber;
-        currentNumber += 1;
-    }
-  }
- console.log(board);
+	console.clear();
+	console.log('Here is the current board...');
+	// prints positions for user
+	let currentNumber = 1;
+	for (let i = 0; i < board.length; i++) {
+		board_position = '';
+		for (let j = 0; j < board[i].length; j++) {
+			board_position += board[i][j] + ' ';
+			currentNumber += 1;
+		}
+		console.log(board_position);
+	}
 }
 
 const markerExists = () => {
-  // checks to see if a marker has already been placed 
+	// checks to see if a marker has already been placed
 }
 
-const placeMarker = (playerMarker) => {
-  let position = prompt('Please enter the position you would like to place your marker, 1-9');
-  // adds X or O to board at specified location  
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-        if (board[i][j] === position && !markerExists()) {
-          currentPlayer = board[i][j];  
-        } else {
-          placeMarker(); 
-        }
-    }
-  }
+const placeMarker = () => {
+	let position = parseInt(prompt('Please enter the position you would like to place your marker, 1-9\n 1 2 3\n 4 5 6\n 7 8 9'));
+	/* 1 2 3
+	   4 5 6
+	   7 8 9 */
+	switch (position) {
+		case 1:
+			board[0][0] = currentPlayer;
+			break;
+		case 2:
+			board[0][1] = currentPlayer;
+			break;
+		case 3:
+			board[0][2] = currentPlayer;
+			break;
+		case 4:
+			board[1][0] = currentPlayer;
+			break;
+		case 5:
+			board[1][1] = currentPlayer;
+			break;
+		case 6:
+			board[1][2] = currentPlayer;
+			break;
+		case 7:
+			board[2][0] = currentPlayer;
+			break;
+		case 8:
+			board[2][1] = currentPlayer;
+			break;
+		case 9:
+			board[2][2] = currentPlayer;
+			break;
+		default:
+			placeMarker();
+			break;
+	}
+	
+	
 }
 
 const switchPlayer = () => {
-  if (currentPlayer === 'X') {
-    currentPlayer = 'O'; 
-  } else {
-    currentPlayer = 'X';
-  }
+	if (currentPlayer === 'X') {
+		currentPlayer = 'O';
+	} else {
+		currentPlayer = 'X';
+	}
 }
 
 const gameWon = () => {
-  // checks to see if any markers span three consecutive cells  
-  // returns true if won, false otherwise
+	// checks to see if any markers span three consecutive cells
+	// returns true if won, false otherwise
 }
 
 const playGame = () => {
-  let startPlayer = Math.floor(Math.random()) > 0.5 ? 'X' : 'O';
-  currentPlayer = startPlayer;
-  while (!gameWon()) {
-    printBoardPositions();
-    placeMarker(currentPlayer);
-    switchPlayer();         
-  }
-  console.log(`Player ${currentPlayer} won!);
+	currentPlayer = Math.random() > 0.5 ? 'X' : 'O'; // Chooses the opening player by random
+	while (!gameWon()) {	// Runs the game till someone wins
+		printBoardPositions();
+		placeMarker();
+		switchPlayer();
+	}
+console.log(`Player ${currentPlayer} won!`);
 }
 
-playGame();
-
-
+//playGame();
